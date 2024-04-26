e6724eea3e11967786e6100038295a7bfe47117a
John Katieno <katienoj@gmail.com>
2024-04-26 12:12:53 +0300

    initial

--- ios/TestReactNativeTests/TestReactNativeTests.m
+++ ios/TestReactNativeTests/TestReactNativeTests.m
@@ -1,18 +1,12 @@
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
-
 #import <React/RCTLog.h>
 #import <React/RCTRootView.h>
-
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
-
 @interface TestReactNativeTests : XCTestCase
-
 @end
-
 @implementation TestReactNativeTests
-
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
   if (test(view)) {
@@ -25,13 +19,11 @@
   }
   return NO;
 }
-
 - (void)testRendersWelcomeScreen
 {
   UIViewController *vc = [[[RCTSharedApplication() delegate] window] rootViewController];
   NSDate *date = [NSDate dateWithTimeIntervalSinceNow:TIMEOUT_SECONDS];
   BOOL foundElement = NO;
-
   __block NSString *redboxError = nil;
 #ifdef DEBUG
   RCTSetLogFunction(
@@ -41,11 +33,9 @@
         }
       });
 #endif
-
   while ([date timeIntervalSinceNow] > 0 && !foundElement && !redboxError) {
     [[NSRunLoop mainRunLoop] runMode:NSDefaultRunLoopMode beforeDate:[NSDate dateWithTimeIntervalSinceNow:0.1]];
     [[NSRunLoop mainRunLoop] runMode:NSRunLoopCommonModes beforeDate:[NSDate dateWithTimeIntervalSinceNow:0.1]];
-
     foundElement = [self findSubviewInView:vc.view
                                   matching:^BOOL(UIView *view) {
                                     if ([view.accessibilityLabel isEqualToString:TEXT_TO_LOOK_FOR]) {
@@ -54,13 +44,10 @@
                                     return NO;
                                   }];
   }
-
 #ifdef DEBUG
   RCTSetLogFunction(RCTDefaultLogFunction);
 #endif
-
   XCTAssertNil(redboxError, @"RedBox error: %@", redboxError);
   XCTAssertTrue(foundElement, @"Couldn't find element with text '%@' in %d seconds", TEXT_TO_LOOK_FOR, TIMEOUT_SECONDS);
 }
-
 @end
